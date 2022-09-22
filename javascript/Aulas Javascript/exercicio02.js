993e451a7c72e2502999e9069ed206e3390596f9
Milianar16 <miliana.rocha16@gmail.com>
2022-09-21 22:02:36 -0300

    Exercicio em JS

---
+++ javascript/Aulas Javascript/exercicio02.js
@@ -0,0 +1,39 @@
+/**
+ * Um posto está vendendo combustíveis com a seguinte tabela de descontos
+ * Álcool - Até 25 litros, descontos de 2% por litro
+ * Acima de 25 litros, desconto de 4% por litro
+ * 
+ * Gasolina -Até 25 litros, desconto de 3% por litro
+ * Acima de 25 litros, desconto de 5% por litro
+ */
+
+
+
+const tipoCombustivel =prompt(`Você irá abastecer com Álcool ou Gasolina? 
+Digite A p/ Álcool 
+Digite G p /Gasolina
+`) 
+
+if (tipoCombustivel == "A") {
+    const litrosAlcool = parseFloat(prompt("Quantos litros foi abastecido?"))
+if(litrosAlcool <= 25){
+    const precoFinal = litrosAlcool * 1.9 - litrosAlcool * 0.2
+    alert(" O valor pago foi de " + precoFinal)
+}else{
+    const precoFinal = litrosAlcool * 1.9 - litrosAlcool * 0.4
+    alert(" O valor pago foi de " + precoFinal)
+}
+    
+}
+
+if(tipoCombustivel == "G"){
+    const litrosGasolina = parseFloat(prompt("Quantos litros foi abastecido?"))
+    if(litrosGasolina <= 25){
+        const precoFinal= litrosGasolina * 2.7 - litrosGasolina * 0.3 
+        alert("O valor pago foi de " + precoFinal)
+    
+    }else{
+        const precoFinal = litrosGasolina * 2.7 - litrosGasolina * 0.5
+        alert(" O valor pago foi de " + precoFinal)
+    }
+}
